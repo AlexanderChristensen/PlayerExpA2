6f8902624d53223a674de794bc1a3cf5b5b7c67e
ShaunShepherd <shaun.shepherd142@gmail.com>
2023-09-13 11:22:45 +1000

    Added Menu

---
+++ PlayerExpA2/Assets/Scripts/Menu.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] bool isMenu;
+
+    [SerializeField] GameObject menuOverlay;
+
+    bool gamePaused;
+
+    void Start()
+    {
+        if (!isMenu)
+        {
+            menuOverlay.SetActive(false);
+        }
+    }
+    void Update()
+    {
+        if (!isMenu)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape)) 
+            {
+                if (!gamePaused)
+                {
+                    menuOverlay.SetActive(true);
+                    Time.timeScale = 0f;
+
+                    Cursor.lockState = CursorLockMode.None;
+                    Cursor.visible = true;
+
+
+                    gamePaused = true;
+                }
+                else
+                {
+                    menuOverlay.SetActive(false);
+                    Time.timeScale = 1f;
+
+                    Cursor.lockState = CursorLockMode.Locked;
+                    Cursor.visible = false;
+
+                    gamePaused = false;
+                }
+            }
+        }
+    }
+    public void UnpauseGame()
+    {
+        Time.timeScale = 1f;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        gamePaused = false;
+
+        menuOverlay.SetActive(false);
+    }
+
+    public void LoadMenu()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        menuOverlay.SetActive(false);
+        Time.timeScale = 1f;
+
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+        if (!isMenu)
+        {
+            menuOverlay.SetActive(false);
+        }
+    }
+}
